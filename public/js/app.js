f9daf05091af74dc6e113398d01ce339f73b0f40
Elyzza Octavo <elyzzacloie@yahoo.com>
2021-08-27 17:47:11 +0800

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,14 +9,6 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-////////////////////////////////////////////////////////////////////////////
-//Goal: Render content to paragraphs
-//1. Select the second message p from JavaScriot
-//2. Just before fetch, render loading message and empty p
-//3. If error, render error
-//4. If no error, render location and forecast
-//5. Tes your work! Search for errors and for valid locations
-
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -25,16 +17,14 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=' + location").then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=' + location").then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
